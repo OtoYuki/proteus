5f342dacb5d592088ce55ea6dfa32016baaf8248
OtoYuki <serotoninotonin@gmail.com>
2025-03-03 17:36:06 +0545

    Add core app models and initial project configuration files

--- core/models.py
+++ core/models.py
@@ -1,3 +1,6 @@
+# Description: This file contains the models for the core app.
 from django.db import models
+import uuid
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
